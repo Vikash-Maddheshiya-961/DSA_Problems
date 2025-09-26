572106fb1b9f066b3f89b463c21bd6d63e8eac17
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2025-09-26 19:25:00 +0530

    Time: 9 ms (49.92%), Space: 7.8 MB (36.11%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
-    int fib_mem(int n,vector<int>& m){
-        int result;
-        if(m[n] == -1){
-            if(n<=1) result = n;
-            else result = fib_mem(n-1,m) + fib_mem(n-2,m);
-            m[n]=result;
-        }
-        return m[n];
-    }
     int fib(int n) {
-        vector<int> m(n+1,-1);
-        return fib_mem(n,m);
+            if(n<=1) return n;
+            return fib(n-1) + fib(n-2);
     }
 };

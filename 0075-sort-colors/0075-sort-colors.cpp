e94c0f379eda614ec1bcf166b277e10762fb0c85
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-06-24 12:06:31 +0530

    Time: 0 ms (100%), Space: 11.6 MB (78.33%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        int no0 = 0;
-        int no1 = 0;
-        int no2 = 0;
-        for(int i=0;i<=n-1;i++){
-            if(nums[i] == 0) no0++;
-            else if(nums[i] == 1) no1++;
-            else no2++;
-        }
-        for(int i=0;i<=n-1;i++){
-            if(i<no0) nums[i] = 0;
-            else if(i<(no0+no1)) nums[i] = 1;
-            else nums[i] = 2;
-        }
+    void swap(int *x,int*y){
+        int temp = *x;
+        *x = *y;
+        *y = temp;
         return;
+    }
+    void sortColors(vector<int>& nums) {
+        // 3 pointer approach 
+        // low = 0, mid = 0 ,high = n-1
+        // 0 to low-1 --> 0 , high+1 to n-1 --> 2
+        // low to mid-1 --> 1 (automatically happen after satisfying the above two conditon)
+        int n = nums.size();
+        int l=0, m=0,h=n-1;
+        while(m<=h){
+            if(nums[m] == 2){
+                swap(&nums[h],&nums[m]);
+                h--;
+            }
+            else if(nums[m]==0){
+                swap(&nums[l],&nums[m]);
+                l++;
+                m++;
+            }
+            else{
+                m++;
+            }
+
+        }
     }
 };

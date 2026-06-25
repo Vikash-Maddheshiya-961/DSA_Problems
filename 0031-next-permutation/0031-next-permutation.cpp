59899c9d6b07be4917144279921f3be47f79624a
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-06-25 10:11:54 +0530

    Time: 0 ms (100%), Space: 15.8 MB (32.68%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,13 +1,5 @@
 class Solution {
 public:
-    void reverse_part(vector<int>& v,int i,int j){
-        while(i<j){
-            swap(v[i],v[j]);
-            i++;
-            j--;
-        }
-        return;
-    }
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int pvt_idx = -1;
@@ -20,11 +12,11 @@ public:
         }
         // checking special case --> when pvt_idx if not found;
         if(pvt_idx == -1){
-            reverse_part(nums,0,n-1);
+            reverse(nums.begin(),nums.end());
             return;
         }
         // 2.Reverse the array from pvt_idx+1 to n-1
-        reverse_part(nums,pvt_idx+1,n-1);
+        reverse(nums.begin()+pvt_idx+1,nums.end());
         // 3.finding just greater element than pvt element
         int jst_max = -1;
         for(int i=pvt_idx+1;i<=n-1;i++){

c4c6102131037cde23d3af3207c02a2233801e91
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-06-28 13:11:50 +0530

    Time: 0 ms (100%), Space: 9.8 MB (9.55%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -3,12 +3,13 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> pascal(numRows);
         for(int i=0;i<numRows;i++){
-            vector<int> v(i+1,1);
+            vector<int> v(i+1);
             pascal[i] = v;
         }
-        for(int i=2 ; i<numRows; i++){
-            for(int j=1; j<i ;j++){
-                pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1];
+        for(int i=0 ; i<numRows; i++){
+            for(int j=0; j<=i ;j++){
+                if(j==0 || j==i) pascal[i][j] = 1;
+                else pascal[i][j] = pascal[i-1][j] + pascal[i-1][j-1];
             }
         }
         return pascal;

b1cc3ae87bf37678a39ec38a897acccfbd3faf74
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-06-29 11:47:17 +0530

    Time: 51 ms (69.52%), Space: 18.6 MB (93.05%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -4,13 +4,10 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         int i=0, j=m-1;
-        while(i<n && i>=0 && j<m && j>=0){
+        while(i<n && j>=0){
             if(matrix[i][j] == target) return true;
-            else if(target < matrix[i][j]){
-                j--;
-            }else{
-                i++;
-            }
+            else if(target < matrix[i][j]) j--;
+            else i++;
         }
         return false;
     }

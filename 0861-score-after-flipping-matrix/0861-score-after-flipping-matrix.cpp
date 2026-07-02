62b48ff7c3730534bee33b34d4adab4679dab48c
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-02 20:05:12 +0530

    Time: 0 ms (100%), Space: 11.4 MB (43.33%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,45 +1,41 @@
 class Solution {
 public:
-    int decimal(vector<int> v){
-        int n = v.size();
-        int dec = 0;
-        int x = 1;
-        for(int i=n-1; i>=0; i--){
-            dec += v[i]*x;
-            x *= 2;
-        }
-        return dec;
-    }
     int matrixScore(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        // 1.all 1's in 0th column by fliping the whole row;
-        for(int i=0; i<m; i++){
+        // checking the first column contain the zeros --> then flip the whole row of that column
+        for(int i=0;i<m;i++){
             if(grid[i][0] == 0){
-                for(int j=0; j<n; j++){
+                for(int j=0;j<n;j++){
                     if(grid[i][j] == 0) grid[i][j] = 1;
                     else grid[i][j] = 0;
                 }
             }
         }
-        //2. Flip the column where no0 > no1
-        for(int j=0; j<n; j++){
+        // flip the column if it contain greater number of zeroes than ones
+        for(int j=0 ;j<n;j++){
             int no0 = 0, no1 = 0;
-            for(int i=0; i<m; i++){
+            for(int i=0;i<m;i++){
                 if(grid[i][j] == 0) no0++;
                 else no1++;
             }
             if(no0 > no1){
-                for(int i=0; i<m; i++){
+                for(int i=0;i<m;i++){
                     if(grid[i][j] == 0) grid[i][j] = 1;
                     else grid[i][j] = 0;
                 }
             }
         }
-        //3. Find score by converting each to decimal
+        // no convert each row to decimal value
         int score = 0;
-        for(int i=0; i<m; i++){
-            score += decimal(grid[i]);
+        for(int i=0;i<m;i++){
+            int bin = 0;
+            int x = 1;
+            for(int j=n-1;j>=0;j--){
+                bin += grid[i][j]*x;
+                x *= 2;
+            }
+            score += bin;
         }
         return score;
     }

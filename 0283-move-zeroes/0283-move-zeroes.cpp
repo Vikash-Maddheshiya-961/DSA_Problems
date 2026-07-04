576a67cd32a48bb8a25ba55e7c3cd110a260a4c0
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-04 11:07:40 +0530

    Time: 216 ms (5.1%), Space: 23.9 MB (55.67%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -2,12 +2,16 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        int j=0;
+        // using bubble sort technique
         for(int i=0;i<n;i++){
-            if(nums[i]!=0){
-                swap(nums[i],nums[j]);
-                j++;
+            bool flag = true;
+            for(int j=0;j<n-1-i;j++){
+                if(nums[j] == 0){
+                    swap(nums[j],nums[j+1]);
+                    flag = false;
+                }
             }
+            if(flag == true) break;
         }
         return;
     }

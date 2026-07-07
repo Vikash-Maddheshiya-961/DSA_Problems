efcb242fb2f6bf3548581cb611a5adffd0c8dc6f
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-07 14:21:43 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return pow(x,0.5);
+        int lo = 1;
+        int hi = x;
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            long long squr = (long long)mid*mid;
+            long long y = (long long)x;
+            if(squr == x) return mid;
+            else if(squr < x) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return hi;
     }
 };

f00e5c52e6d3275c24b4e583f5d9bdfa8cead587
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-07 12:15:29 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.33%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         int lo = 0, hi = n-1;
         while(lo <= hi){
-            int mid = (lo+hi)/2;
+            int mid = lo + (hi-lo)/2;
             if(nums[mid] == target) return mid;
             else if(nums[mid] < target) lo = mid+1;
             else hi = mid-1;

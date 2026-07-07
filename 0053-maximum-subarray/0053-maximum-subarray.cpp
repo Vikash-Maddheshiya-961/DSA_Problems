39e0e994960660ddffab58a5906093b267609507
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-07 10:52:47 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.93%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int max_sum = INT_MIN;
         int curr_sum = 0;
         for(int i=0;i<n;i++){
-            curr_sum = curr_sum + nums[i];
+            curr_sum += nums[i];
             max_sum = max(curr_sum,max_sum);
             if(curr_sum < 0){
                 curr_sum = 0;

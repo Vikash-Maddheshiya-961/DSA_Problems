d0b58c6e9408b30785563e8f6c2341bdb1f96dd3
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-08 23:57:59 +0530

    Time: 4 ms (23.12%), Space: 7.9 MB (20.9%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,27 +1,10 @@
 class Solution {
 public:
-    long long sroot(long long x){
-        long long lo = 1;
-        long long hi = x;
-        while(lo <= hi){
-            long long mid = lo + (hi-lo)/2;
-            if((mid*mid) == x){
-                return mid;
-            }
-            else if((mid*mid) < x){
-                lo = mid + 1;
-            }
-            else{
-                hi = mid - 1;;
-            }
-        }
-        return hi;
-    }
     bool judgeSquareSum(int c) {
         long long k = (long long)c;
-        long long i = sroot(c);
+        long long i = sqrt(k);
         for(long long b=0;b<=i;b++){
-            long long a = sroot(k - (b*b));
+            long long a = sqrt(k - (b*b));
             if(((a*a) + (b*b)) == k){
                 return true;
             }

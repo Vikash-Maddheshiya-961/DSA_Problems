a0f0a71bcae6560bbde6343083f74d70dc51155c
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-10 14:08:28 +0530

    Time: 0 ms (100%), Space: 8.7 MB (19.48%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    long long total_ways(int n,vector<long long>& ways){
+    int climbStairs(int n) {
         if(n==1) return 1;
         if(n==2) return 2;
-        if(ways[n] !=0 ) return ways[n];
-        ways[n] = total_ways(n-1,ways) + total_ways(n-2,ways);
+        vector<int> ways(n+1);
+        ways[1] = 1;
+        ways[2] = 2;
+        for(int i=3;i<=n;i++){
+            ways[i] = ways[i-1] + ways[i-2];
+        }
         return ways[n];
-    }
-    int climbStairs(int n) {
-        vector<long long> ways(n+1,0);
-        return (int)total_ways(n,ways);
     }
 };

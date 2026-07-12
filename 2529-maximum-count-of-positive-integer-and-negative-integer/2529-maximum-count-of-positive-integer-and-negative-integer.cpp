ea1d1e1d7da54d6bb5a1f6659e9e5307b22d1434
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-12 20:55:46 +0530

    Time: 0 ms (100%), Space: 21.4 MB (62.27%) - LeetHub

--- 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,13 +1,45 @@
 class Solution {
 public:
     int maximumCount(vector<int>& nums) {
-        int pos = 0;
-        int neg = 0;
-        for(int val : nums){
-            if (val == 0) continue;
-            else if(val < 0) neg++;
-            else pos++;
+        int n = nums.size();
+        int lo = 0, hi = n-1;
+        int lb_zero = -1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == 0){
+                if(mid >= 1 && nums[mid] == nums[mid-1]){
+                    hi = mid-1;
+                }
+                else{
+                    lb_zero = mid;
+                    break;
+                }
+            }
+            else if(nums[mid] < 0) lo = mid + 1;
+            else hi = mid - 1;
         }
-        return max(pos,neg);
+
+        if(lb_zero == -1) {
+            return max(n-lo,lo);
+        }
+
+        int ub_zero = -1;
+        lo = 0;
+        hi = n-1;
+        while(lo <= hi){
+            int mid = lo + (hi - lo) / 2;
+            if(nums[mid] == 0){
+                if(mid <= n-2 && nums[mid] == nums[mid+1]){
+                    lo = mid+1;
+                }
+                else{
+                    ub_zero = mid;
+                    break;
+                }
+            }
+            else if(nums[mid] < 0) lo = mid + 1;
+            else hi = mid - 1;
+        }
+        return max(n-1-ub_zero,lb_zero);
     }
 };

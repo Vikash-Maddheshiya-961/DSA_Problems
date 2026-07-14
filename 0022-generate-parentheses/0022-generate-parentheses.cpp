4d35e2e2af7edc2f25a0e97d29d5f9827c192110
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-14 23:20:38 +0530

    Time: 4 ms (33.38%), Space: 15.6 MB (65.47%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     void solve(vector<string>& res,string s,int op,int cl,int n){
-        if(op == n && cl == n){
+        if(cl == n){
             res.push_back(s);
             return;
         }
-        if(op<n){
-            solve(res,s+"(",op+1,cl,n);
-        }
-        if(cl < op){
-            solve(res,s+")",op,cl+1,n);
-        }
+        if(op<n) solve(res,s+"(",op+1,cl,n);
+        if(cl < op) solve(res,s+")",op,cl+1,n);
         return;
     }
     vector<string> generateParenthesis(int n) {

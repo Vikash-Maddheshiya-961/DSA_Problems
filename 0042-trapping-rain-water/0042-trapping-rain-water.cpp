81cd8766f6dfebc24a068da9d07a62731bcb39e7
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-14 21:05:18 +0530

    Time: 0 ms (100%), Space: 27.4 MB (6.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -2,31 +2,23 @@ class Solution {
 public:
     int trap(vector<int>& height) {
         int n = height.size();
-        // Previous_Greatest_Element Array
-        vector<int> PGE(n);
-        PGE[0] = -1;
-        // next_Greatest_Element Array
-        vector<int> NGE(n);
-        NGE[n-1] = 0;
-        //Filling previous_Element Array
-        int max = height[0];
-        for(int i=1;i<=n-1;i++){
-            PGE[i] = max;
-            if(max<height[i]) max = height[i];
+        vector<int> prev_max(n,-1);
+        vector<int> nxt_max(n,-1);
+        int maxi = height[0];
+        for(int i=1;i<n;i++){
+            prev_max[i] = maxi; 
+            if(maxi < height[i]) maxi = height[i];
         }
-        //Filling Next_Element Array
-        max = height[n-1];
+        maxi = height[n-1];
         for(int i=n-2;i>=0;i--){
-            NGE[i] = max;
-            if(max<height[i]) max = height[i];
+            nxt_max[i] = maxi;
+            if(maxi < height[i]) maxi = height[i];
         }
-        // Now calculating the amount of trap water
         int water = 0;
-        int mini;
         for(int i=1;i<=n-2;i++){
-            mini = min(PGE[i],NGE[i]);
+            int mini = min(prev_max[i],nxt_max[i]);
             if(height[i] < mini){
-                water += mini-height[i];
+                water += mini - height[i];
             }
         }
         return water;

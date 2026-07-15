69109800a2dd13a457befbd08e49fa0ee77611ee
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-15 13:39:26 +0530

    Time: 1 ms (91.89%), Space: 9 MB (94.77%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,45 +1,31 @@
 class Solution {
 public:
-    string helper2(vector<vector<int>> res){
-        string s = "";
-        for(auto v:res){
-            for(int i = v.size()-1;i>=0;i--){
-                s += (char)(v[i] + 48);
-            }
-        }
-        return s;
-    }
-    vector<vector<int>> helper1(string s){
-        int len = s.length();
-        vector<vector<int>> res;
-        vector<int> ans;
-        ans.push_back(s[0]-'0');
-        int count = 1;
-        int curr;
-        int prev = s[0]-'0';
-        for(int i=1;i<=len;i++){
-            if(i != len) curr = s[i] - '0';
-            if(i!=len && prev!=curr){
-                ans.push_back(count);
-                res.push_back(ans);
-                count = 0;
-                while(ans.size()!=0){
-                    ans.pop_back();
-                }
-                ans.push_back(curr);
-                count++;
-                prev = curr;
-            }else if(i!=len && prev == curr){
-                count++;
-            }else{
-                ans.push_back(count);
-                res.push_back(ans);
-            }
-        }
-        return res;
-    }
     string countAndSay(int n) {
         if(n==1) return "1";
-        return helper2(helper1(countAndSay(n-1)));
+        string str = countAndSay(n-1);
+        string ztr;
+        ztr.push_back(str[0]);
+        int count = 1;
+        char prev = str[0];
+        for(int i=1;i<str.size();i++){
+            char curr = str[i];
+            if(prev == curr){
+                count++;
+            }
+            else{
+                char temp = ztr[ztr.length()-1];
+                ztr.pop_back();
+                ztr.push_back(char(count+48)); 
+                ztr.push_back(temp);
+                ztr.push_back(curr);
+                count = 1;
+                prev = curr;
+            }
+        }
+        char temp = ztr[ztr.length()-1];
+        ztr.pop_back();
+        ztr.push_back(char(count+48)); 
+        ztr.push_back(temp);
+        return ztr;
     }
 };

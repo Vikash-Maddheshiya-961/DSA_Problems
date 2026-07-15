7d5d2e13abf26adb82df1c3c37df9b1bb18b9640
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-15 21:11:43 +0530

    Time: 0 ms (100%), Space: 15.7 MB (40.47%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,31 +2,26 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
-        int pvt_idx = -1;
-        // 1.Find pivot index
+        int pivot = -1;
         for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                pvt_idx = i;
+            if(nums[i] < nums[i+1]){
+                pivot = i;
                 break;
             }
         }
-        // checking special case --> when pvt_idx if not found;
-        if(pvt_idx == -1){
+        if(pivot == -1){
             reverse(nums.begin(),nums.end());
             return;
         }
-        // 2.Reverse the array from pvt_idx+1 to n-1
-        reverse(nums.begin()+pvt_idx+1,nums.end());
-        // 3.finding just greater element than pvt element
+        reverse(nums.begin()+pivot+1,nums.end());
         int jst_max = -1;
-        for(int i=pvt_idx+1;i<=n-1;i++){
-            if(nums[i]>nums[pvt_idx]){
+        for(int i=pivot+1;i<n;i++){
+            if(nums[i]>nums[pivot]){
                 jst_max = i;
                 break;
             }
         }
-        // 4.swap pivot and jst_maximum element
-        swap(nums[pvt_idx],nums[jst_max]);
+        swap(nums[pivot],nums[jst_max]);
         return;
     }
 };

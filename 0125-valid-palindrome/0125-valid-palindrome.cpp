90d99dbcf07108a7778899735931239d81e09a52
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-16 11:21:23 +0530

    Time: 0 ms (100%), Space: 10.1 MB (37.74%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string str;
+        for(int i=0;i<s.length();i++){
+            if((s[i] >= 'a' && s[i] <= 'z') || (s[i] >='0' && s[i] <= '9')){
+                str.push_back(s[i]);
+            }
+            else if(s[i] >= 'A' && s[i] <= 'Z'){
+                int idx = s[i] - 'A';
+                char ch = char(idx+97);
+                str.push_back(ch);
+            }
+        }
+        cout << str;
+        int i=0, j=str.length()-1;
+        while(i<j){
+            if(str[i]!=str[j]) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+};

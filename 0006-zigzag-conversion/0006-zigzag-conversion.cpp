3b25b3ae6ced560c6f92cc1a48922149a715e52b
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-16 10:49:22 +0530

    Time: 154 ms (5.1%), Space: 88.2 MB (5%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
     string convert(string s, int numRows) {
-        vector<string> arr(numRows);
-        for(int i=0;i<numRows;i++){
-            arr[i]="";
-        }
-        int i=0;
-        while(i<s.size()){
-            for(int row=0;row<numRows && i<s.size();row++){
-                arr[row]+=s[i++];
+        int n = s.length();
+        if(numRows >= n || numRows == 1) return s;
+        vector<vector<char>> grid(numRows,vector<char>(1000,'0'));
+        int i = 0;
+        int r = 0;
+        int c = 0;
+        while(i<n){
+            while(r<numRows && i<n){
+                grid[r++][c] = s[i++];
             }
-            for(int rev=numRows-2;rev>0 && i<s.size();rev--){
-                arr[rev]+=s[i++];
+            r--;
+            while(r!=1 && i<n){
+                grid[--r][++c] = s[i++];
             }
+            r--;
+            c++;
         }
         string ans;
-        for(string c:arr){
-            ans+=c;
+        int count = 0;
+        for(int i=0;i<numRows;i++){
+            for(int j=0;j<1000;j++){
+                if(grid[i][j] !='0'){
+                    ans.push_back(grid[i][j]);
+                    count++;
+                }
+                if(count == n) break;
+            }
+            if(count == n) break;
         }
         return ans;
     }

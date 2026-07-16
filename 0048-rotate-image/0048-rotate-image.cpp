fe4381655e4b7f74e316c5524a81f6ccc2e082f5
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-17 01:04:24 +0530

    Time: 0 ms (100%), Space: 10 MB (93.88%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        int n = matrix.size(); // order of square matrix
-        // 1.Transpose of square matrix
-        for(int i=0; i<=n-1; i++){
-            for(int j=i+1; j<=n-1; j++){
+        int n = matrix.size();
+        for(int i=0; i<n; i++){
+            for(int j=i+1;j<n;j++){
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
-        // 2.Reverse each row;
-        for(int i=0; i<=n-1; i++){
-            int j=0,k=n-1;
-            while(j<k){
-                swap(matrix[i][j],matrix[i][k]);
-                j++;
-                k--;
-            }
+        for(auto& v:matrix){
+            reverse(v.begin(),v.end());
         }
         return;
     }

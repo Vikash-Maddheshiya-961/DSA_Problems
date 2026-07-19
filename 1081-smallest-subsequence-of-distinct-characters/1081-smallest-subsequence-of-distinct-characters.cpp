a05f9dca73f9e270deecdf57644deeb47519663a
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-07-19 20:49:12 +0530

    Time: 0 ms (100%), Space: 8.6 MB (94.04%) - LeetHub

--- 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.cpp
@@ -4,25 +4,24 @@ public:
         int n = s.length();
         vector<int> idx(26);
         vector<int> seen(26,0);
-
+        
         for(int i=0;i<n;i++){
-            idx[s[i]-'a'] = i;
+            idx[s[i] - 'a'] = i;
         }
 
         string ans;
 
         for(int i=0;i<n;i++){
             char ch = s[i];
-            if(seen[ch - 'a']) continue;
+            if(seen[ch - 'a'] == 1) continue;
 
             while(ans.length()!=0 && ans.back() > ch && idx[ans.back()-'a'] > i){
-                seen[ans.back() - 'a'] = 0;
+                seen[ans.back()-'a'] = 0;
                 ans.pop_back();
             }
             ans.push_back(ch);
             seen[ch-'a'] = 1;
         }
-        
         return ans;
     }
 };

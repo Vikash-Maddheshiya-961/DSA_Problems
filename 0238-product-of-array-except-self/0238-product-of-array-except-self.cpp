d2ae03ceaa6a3ad0b63e8ab4e87b86900bb07622
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-01 02:42:19 +0530

    Time: 0 ms (100%), Space: 42.3 MB (8.4%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -12,10 +12,9 @@ public:
         for(int i=n-2;i>=0;i--){
             suffix[i] = suffix[i+1]*nums[i+1];
         }
-        vector<int> ans(n);
         for(int i=0;i<n;i++){
-            ans[i] = prefix[i] * suffix[i];
+            nums[i] = prefix[i] * suffix[i];
         }
-        return ans;
+        return nums;
     }
 };

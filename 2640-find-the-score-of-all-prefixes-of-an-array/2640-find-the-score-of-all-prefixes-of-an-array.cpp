dbad146e3a7b6f6faa0847625e9c5dab1efb80d0
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-02 00:19:19 +0530

    Time: 4 ms (31.18%), Space: 59 MB (75.27%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.cpp
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     vector<long long> findPrefixScore(vector<int>& nums) {
         int n = nums.size();
-        // prefix max + conversion array
-        int maxi = INT_MIN;
-        for(int i=0;i<n;i++){
-            if(maxi < nums[i]) maxi = nums[i]; 
-            nums[i] += maxi;
-        }
-        // now prefix sum
         vector<long long> ans(n);
+        // prefix max + conversion array
+        int maxi = nums[0];
+        nums[0] *= 2;
         ans[0] = nums[0];
         for(int i=1;i<n;i++){
+            if(maxi < nums[i]) maxi = nums[i]; 
+            nums[i] += maxi;
             ans[i] = ans[i-1] + nums[i];
         }
         return ans;

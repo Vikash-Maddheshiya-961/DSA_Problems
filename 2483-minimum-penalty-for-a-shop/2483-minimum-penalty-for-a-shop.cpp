bb3199d396443a9148e6cc7aae46443d216106dd
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-01 11:01:29 +0530

    Time: 14 ms (18.86%), Space: 21.7 MB (38.1%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n = customers.size();
-        vector<int> N_before(n+1);
-        N_before[0] = 0;
+        int n = customers.length();
+        vector<int> pre(n+1); //  Number of N before Kth hour
+        pre[0] = 0;
         for(int i=1;i<=n;i++){
+            int count = 0;
             if(customers[i-1] == 'N'){
-                N_before[i] = N_before[i-1]+1;
-            }else{
-                N_before[i] = N_before[i-1];
+                count++;
             }
+            pre[i] = pre[i-1] + count;
         }
-        vector<int> Y_after(n+1);
-        Y_after[n] = 0;
+        vector<int> suf(n+1); //  Number of Y after and including Kth hour
+        suf[n] = 0;
         for(int i=n-1;i>=0;i--){
+            int count = 0;
             if(customers[i] == 'Y'){
-                Y_after[i] = Y_after[i+1] + 1;
+                count++;
             }
-            else{
-                Y_after[i] = Y_after[i+1];
-            }
+            suf[i] = suf[i+1] + count;
         }
         // finding penaltyi for each hour
         int min_penalty = INT_MAX;
         for(int i=0;i<=n;i++){
-            N_before[i] = N_before[i] + Y_after[i];
-            if(min_penalty > N_before[i]) min_penalty = N_before[i];
+            pre[i] = pre[i] + suf[i];
+            if(min_penalty > pre[i]) min_penalty = pre[i];
         }
         // now finding the earliest shop closing time
         for(int i=0;i<=n;i++){
-            if(N_before[i] == min_penalty){
+            if(pre[i] == min_penalty){
                 return i;
             }
         }

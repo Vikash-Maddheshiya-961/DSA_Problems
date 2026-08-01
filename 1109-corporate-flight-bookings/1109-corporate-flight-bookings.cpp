f396bde350e57b17da91fb25767c4e2fa8d0fdca
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-02 01:12:17 +0530

    Time: 0 ms (100%), Space: 72 MB (89.79%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.cpp
@@ -3,15 +3,17 @@ public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
         int r = bookings.size();
         int c = bookings[0].size();
-        vector<int> ans(n,0);
+        vector<int> ans(n);
         for(int i=0;i<r;i++){
-            int s = bookings[i][0]-1;
-            int e = bookings[i][1]-1;
-            int seat = bookings[i][2];
-            while(s<=e){
-                ans[s] += seat;
-                s++;
-            }
+            int s = bookings[i][0] - 1;
+            int e = bookings[i][1] - 1;
+            int p = bookings[i][2];
+            ans[s] += p;
+            if(e == n-1) continue;
+            ans[e+1] -= p; 
+        }
+        for(int i=1;i<n;i++){
+            ans[i] += ans[i-1];
         }
         return ans;
     }

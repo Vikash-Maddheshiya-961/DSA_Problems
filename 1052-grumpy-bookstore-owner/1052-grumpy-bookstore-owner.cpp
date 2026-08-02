014da1a57d14ad7d29cafb9c5e1a08f4fc867735
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-02 11:12:50 +0530

    Time: 0 ms (100%), Space: 35.9 MB (44.81%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -2,26 +2,33 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int n = customers.size();
-        int sc = 0;
-        for(int i=0;i<n;i++){
-            if(grumpy[i] == 0) sc += customers[i];
-        }
-        int prev_sc = sc;
+        int prev_los = 0; //  previous loss of satisfation
         for(int i=0;i<minutes;i++){
-            if(grumpy[i] == 1) prev_sc += customers[i];
+            if(grumpy[i] == 1) prev_los += customers[i];
         }
-        int max_sc = prev_sc;
-        int i=1;
-        int j=minutes;
+        int idx = 0;
+        int max_los = prev_los; //  maximum loss of satisfaction
+        int i = 1;
+        int j = minutes;
         while(j<n){
-            int curr_sc = prev_sc;
-            if(grumpy[i-1] == 1) curr_sc -= customers[i-1];
-            if(grumpy[j] == 1) curr_sc += customers[j];
-            max_sc = max(max_sc,curr_sc);
-            prev_sc = curr_sc;
+            int curr_los = prev_los;
+            if(grumpy[i-1] == 1) curr_los -= customers[i-1];
+            if(grumpy[j] == 1) curr_los += customers[j];
+            if(curr_los > max_los){
+                max_los = curr_los;
+                idx = i;
+            }
+            prev_los = curr_los;
             i++;
             j++;
         }
-        return max_sc;
+        for(int i=idx;i<=idx+minutes-1;i++){
+            if(grumpy[i] == 1) grumpy[i] = 0;
+        }
+        int max_s = 0;
+        for(int i=0;i<n;i++){
+            if(grumpy[i] == 0) max_s += customers[i];
+        }
+        return max_s;
     }
 };

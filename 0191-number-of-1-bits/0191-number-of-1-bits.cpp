fba9594723987e71f476b93102b33e66f20f167e
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-03 23:58:48 +0530

    Time: 0 ms (100%), Space: 8.1 MB (83.09%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,6 @@
 class Solution {
 public:
     int hammingWeight(int n) {
-        int count = 0;
-        while(n > 0){
-            n = n & (n - 1);
-            count++;
-        }
-        return count;
+        return __builtin_popcount(n);
     }
 };

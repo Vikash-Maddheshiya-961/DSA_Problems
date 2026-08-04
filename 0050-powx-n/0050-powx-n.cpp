9ea152f0b01852b136946a0551bf5ad8eb580c39
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-04 21:44:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (71.16%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -3,14 +3,11 @@ public:
     double myPow(double x, int n) {
         if(n == 0) return 1;
         double a = myPow(x,n/2);
-        if(n < 0 && n%2 == 0){
+        if(n%2 == 0){
             return a * a;
         }
         if(n < 0 && n%2 !=0 ){
             return a * a * (1 / x);
-        }
-        if(n > 0 && n%2 == 0){
-            return a * a;
         }
         return a * a * x;
     }

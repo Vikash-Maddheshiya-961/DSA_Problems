961aa128b02dee8f992e824853b058eb801cf38c
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-08-05 11:23:49 +0530

    Time: 3 ms (64.78%), Space: 8.2 MB (16.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    vector<int> res;
-    int fibo(int n){
-        if(n<=1) return n;
-        if(res[n] != -1) return res[n];
-        return res[n] = fibo(n-1) + fibo(n-2);
-    }
     int fib(int n) {
-        res.assign(n+1,-1);
-        return fibo(n);
+        if(n <= 1) return n;
+        vector<int> fibo(n+1,-1);
+        fibo[0] = 0;
+        fibo[1] = 1;
+        for(int i=2;i<=n;i++){
+            fibo[i] = fibo[i-1] + fibo[i-2]; 
+        }
+        return fibo[n];
     }
 };

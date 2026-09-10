6ab4bfbc8dfea7e973561e08f570e948677664f4
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-10 14:32:14 +0530

    Time: 0 ms (100%), Space: 23.9 MB (51.45%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,28 +2,49 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        sort(nums.begin(),nums.end());
-        int count = n/3 + 1;
-        vector<int> ans;
 
-        int curr_count = 0;
-        int ele = nums[0];
+        int candidate1 = INT_MIN;
+        int candidate2 = INT_MIN;
+
+        int count1 = 0, count2 = 0;
+
+        // finding potential candidates
         for(int i=0;i<n;i++){
-            if(nums[i] != ele){
-                if(curr_count >= count){
-                    ans.push_back(ele);
-                }
-                curr_count = 1;
-                ele = nums[i];
+            if(nums[i] == candidate1){
+                count1++;
+            }
+            else if(nums[i] == candidate2){
+                count2++;
+            }
+            else if(count1 == 0){
+                candidate1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                candidate2 = nums[i];
+                count2 = 1;
             }
             else{
-                curr_count++;
+                count1--;
+                count2--;
             }
         }
 
-        if(curr_count >= count){
-            ans.push_back(ele);
+        // validation
+        count1 = 0, count2 = 0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == candidate1) count1++;
+            else if(nums[i] == candidate2) count2++;
         }
+
+        vector<int> ans;
+
+        int freq = n/3;
+
+        if(count1 > freq) ans.push_back(candidate1);
+        if(count2 > freq) ans.push_back(candidate2);
+
         return ans;
     }
 };

e9f33100b0c409776714aed340a070dbdb152149
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-13 01:48:09 +0530

    Time: 3 ms (34.18%), Space: 77.1 MB (75.81%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,25 +11,67 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res = new ListNode(0);
-        ListNode *ptr = res;
+        ListNode* ptr1 = l1;
+        ListNode* ptr2 = l2;
+        ListNode* head = NULL;
+        ListNode* ptr = NULL;
         int carry = 0;
-        while(l1!=NULL || l2!=NULL || carry!=0){
-            int val1 = (l1!=NULL) ? l1->val:0;
-            int val2 = (l2!=NULL) ? l2->val:0;
-            int sum = val1+val2+carry;
-            carry = sum/10;
-            int digit = sum%10;
-            ptr->next = new ListNode(digit);
-            ptr = ptr->next;
 
-            if(l1!=NULL){
-                l1 = l1->next;
+        while(ptr1 && ptr2){
+            int sum = ptr1->val + ptr2->val + carry;
+            int fraction = sum % 10;
+            carry = sum / 10;
+            ListNode* temp = new ListNode(fraction);
+            if(head == NULL){
+                head = temp;
+                ptr = head;
             }
-            if(l2!=NULL){
-                l2 = l2->next;
+            else{
+                ptr -> next = temp;
+                ptr = ptr->next;
             }
+            ptr1 = ptr1->next;
+            ptr2 = ptr2->next;
         }
-        return res->next;
+
+        while(ptr1){
+            int sum = ptr1->val + carry;
+            int fraction = sum % 10;
+            carry = sum / 10;
+            ListNode* temp = new ListNode(fraction);
+            if(head == NULL){
+                head = temp;
+                ptr = head;
+            }
+            else{
+                ptr -> next = temp;
+                ptr = ptr->next;
+            }
+            ptr1 = ptr1->next;
+        }
+
+        while(ptr2){
+            int sum = ptr2->val + carry;
+            int fraction = sum % 10;
+            carry = sum / 10;
+            ListNode* temp = new ListNode(fraction);
+            if(head == NULL){
+                head = temp;
+                ptr = head;
+            }
+            else{
+                ptr -> next = temp;
+                ptr = ptr->next;
+            }
+            ptr2 = ptr2->next;
+        }
+
+        if(carry != 0){
+            ListNode* temp = new ListNode(carry);
+            ptr -> next = temp;
+            ptr = ptr -> next;
+        }
+
+        return head;
     }
 };

2386585ee279d39e65f0a2774b11af36a402d4db
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-13 00:10:30 +0530

    Time: 0 ms (100%), Space: 22.8 MB (78.05%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,33 +10,54 @@
  */
 class Solution {
 public:
-    ListNode* reverse_list(ListNode* head){
-        ListNode* prev = NULL;
-        ListNode* curr = head;
+    void reorderList(ListNode* head) {
+        if(!head || !head->next || !head->next->next) return;
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
 
-        while(curr != NULL){
-            ListNode *nxt = curr -> next;
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow -> next;
+            fast = fast -> next -> next;
+
+            if(slow == fast) break;
+        }
+
+        ListNode* pointer = head;
+
+        while(pointer->next != slow){
+            pointer = pointer->next;
+        }
+        pointer-> next = NULL;
+
+        ListNode* prev = NULL;
+        ListNode* curr = slow;
+
+        while(curr!=NULL){
+            ListNode* nxt = curr->next;
             curr -> next = prev;
             prev = curr;
             curr = nxt;
         }
 
-        return prev;
-    }
-    void reorderList(ListNode* head) {
-        if(head == NULL || head->next == NULL || head->next->next == NULL) return;
+        ListNode* ptr1 = head;
+        ListNode* ptr2 = prev;
 
-        ListNode *prev = head;
-        ListNode *curr = head->next;
-
-        while(curr -> next != NULL){
-            ListNode *ptr = reverse_list(curr);
-            prev -> next = ptr;
-            curr = ptr;
-            prev = curr;
-            curr = curr -> next;
+        while(ptr1 != NULL && ptr2 != NULL){
+            ListNode *temp = ptr1->next;
+            ptr1->next = ptr2;
+            ptr1 = temp;
+            temp = ptr2->next;
+            ptr2->next = ptr1;
+            ptr2 = temp;
         }
 
+        if(ptr2!=NULL){
+            ptr1 = head;
+            while(ptr1 -> next != NULL) ptr1 = ptr1 -> next;
+            ptr1 -> next = ptr2;
+        }
+        
         return;
     }
 };

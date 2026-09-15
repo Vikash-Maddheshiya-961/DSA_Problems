be3507447f36b7eab4b20e79559175adb8f5200f
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-15 16:58:55 +0530

    Time: 0 ms (100%), Space: 34.4 MB (15.33%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -1,5 +1,5 @@
 /**
- * Definition for a binary tree node.
+ * Definition for a binary tree curr.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -11,36 +11,83 @@
  */
 class Solution {
 public:
-    TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL) return root;
-        if(key < root->val){
-            root->left = deleteNode(root->left,key);
-        }else if(key>root->val){
-            root->right = deleteNode(root->right,key);
-        }else{
-            if(root->left == NULL){
-                TreeNode* temp = root->right;
-                root->right = NULL;
-                delete root;
-                return temp;
-            }
-            else if(root->right == NULL){
-                TreeNode* temp = root->left;
-                root->left = NULL;
-                delete root;
-                return temp;
-            }else{
-                int val = min_val(root->right);
-                root->val = val;
-                root->right = deleteNode(root->right,val);
-            }
-        }
-        return root;
+    int findsucc(TreeNode* curr){
+        while(curr -> left) curr = curr -> left;
+        return curr -> val;
     }
-    int min_val(TreeNode* temp){
-        while(temp->left!=NULL){
-            temp = temp->left;
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root) return root;
+        TreeNode* temp = root;
+        TreeNode* curr = NULL;
+        TreeNode* prev = NULL;
+        while(temp){
+            if(key == temp->val){
+                curr = temp;
+                break;
+            }
+            prev = temp;
+            if(key < temp->val) temp = temp -> left;
+            else temp = temp -> right;
         }
-        return temp->val;
+        if(curr == NULL) return root;
+
+        // no child
+
+        if(!curr -> left && !curr -> right){
+            if(prev == NULL) {
+                delete curr;
+                return NULL;
+            }
+            if(curr == prev ->left) prev -> left = NULL;
+            else prev -> right = NULL;
+            delete curr;
+            return root;
+        }
+
+        // 1 child
+        if(curr->left && !curr->right){
+            if(prev == NULL){
+                prev = curr -> left;
+                curr -> left = NULL;
+                delete curr;
+                return prev;
+            }
+
+            if(curr == prev -> right){
+                prev -> right = curr -> left;
+            }
+            else{
+                prev -> left = curr -> left;
+            }
+            curr -> left = NULL;
+            delete curr;
+            return root;
+        }
+
+        if(!curr -> left && curr -> right){
+            if(prev == NULL){
+                prev = curr -> right;
+                curr -> right = NULL;
+                delete curr;
+                return prev;
+            }
+
+            if(curr == prev->left){
+                prev -> left = curr -> right;
+            }else{
+                prev -> right = curr -> right;
+            }
+
+            curr -> right = NULL;
+            delete curr;
+            return root;
+        }
+
+        // 2 child
+        int successor = findsucc(curr->right);
+        curr -> val = successor;
+        curr -> right = deleteNode(curr->right, successor);
+
+        return root;
     }
 };

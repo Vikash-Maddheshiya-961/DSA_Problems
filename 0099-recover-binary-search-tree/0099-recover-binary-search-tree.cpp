985ef02e1d81b1a0cb5f564ec124054754653dc0
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-17 01:40:09 +0530

    Time: 0 ms (100%), Space: 60.8 MB (78.4%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -10,29 +10,32 @@
  * };
  */
 class Solution {
-public:
-    void inorder(TreeNode* root,vector<int> &v){
+private:
+    TreeNode* first;
+    TreeNode* middle;
+    TreeNode* last;
+    TreeNode* prev;
+    void inorder(TreeNode* root){
         if(root == NULL) return;
-        inorder(root->left,v);
-        v.push_back(root->val);
-        inorder(root->right,v);
-        return;
-    }
-    void solve(TreeNode* root,vector<int> &v,int &idx){
-        if(root == NULL) return;
-        solve(root->left,v,idx);
-        if(v[idx]!=root->val){
-            root->val = v[idx++];
+        
+        inorder(root->left);
+
+        if(prev && root->val < prev->val){
+            if(first == NULL){
+                first = prev;
+                middle = root;
+            }
+            else last = root;
         }
-        else idx++;
-        solve(root->right,v,idx);
-        return;
+
+        prev = root;
+        inorder(root->right);
     }
+public:
     void recoverTree(TreeNode* root) {
-        vector<int> v;
-        inorder(root,v);
-        sort(v.begin(),v.end());
-        int idx = 0;
-        solve(root,v,idx);
+        inorder(root);
+        if(first && last) swap(first->val,last->val);
+        else if(first && middle) swap(first->val,middle->val);
+        return;
     }
 };

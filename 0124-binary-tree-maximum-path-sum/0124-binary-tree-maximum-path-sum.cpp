8659b042c88e2842d8b962a2a27dfc6efa7ea7af
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-18 00:50:27 +0530

    Time: 0 ms (100%), Space: 28 MB (21.99%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -13,10 +13,13 @@ class Solution {
 public:
     int solve(TreeNode* root,int &maxi){
         if(!root) return 0;
+
         int left = max(0,solve(root->left,maxi));
         int right = max(0,solve(root->right,maxi));
-        maxi = max(maxi, root->val + left + right);
-        return root -> val + max(left,right);
+
+        maxi = max(maxi,root->val + left + right);
+
+        return root->val + max(left,right);
     }
     int maxPathSum(TreeNode* root) {
         int maxi = INT_MIN;

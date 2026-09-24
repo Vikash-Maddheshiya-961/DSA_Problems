bdcd75c6974fb502a9ecdb433ca85f8b765ed359
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-24 21:46:39 +0530

    Time: 136 ms (34.93%), Space: 19.5 MB (57.62%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    bool diffBy1(string &s1,string &s2){
+        int n1 = s1.length();
+        int n2 = s2.length();
+        if(n2-n1 != 1){
+            return false;
+        }
+        int i=0,j=0;
+        int diff = 0;
+        while(i<n1 && j<n2){
+            if(s1[i] != s2[j]){
+                diff++;
+                j++;
+            }
+            else{
+                i++;
+                j++;
+            }
+            if(diff > 1) return false;
+        }
+
+        return true;
+    }
+    int longestStrChain(vector<string>& words) {
+        int n = words.size();
+        sort(words.begin(),words.end(),[](string &s1,string &s2){
+            return s1.length() < s2.length();
+        });
+        for(string s:words) cout << s << " ";
+        cout << endl;
+        vector<int> dp(n,1);
+        int maxi = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(diffBy1(words[j],words[i]) && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            maxi = max(maxi,dp[i]);
+        }
+        for(int val:dp) cout<< val << " ";
+        cout << endl;
+        return maxi;
+    }
+};

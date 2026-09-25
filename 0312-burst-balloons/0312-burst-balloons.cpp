6adcb536b8dc0e1f4190df38f4a7edf613077019
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-26 00:59:13 +0530

    Time: 155 ms (53.98%), Space: 11 MB (98.19%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int res[301][301];
+    int solve(vector<int>& nums,int i,int j){
+        if(i > j) return 0;
+
+        if(res[i][j] != -1) return res[i][j];
+        int max_coin = INT_MIN;
+        for(int k=i;k<=j;k++){
+
+            int curr_coin = nums[i-1]*nums[k]*nums[j+1] + solve(nums,i,k-1) + solve(nums,k+1,j);
+            max_coin = max(max_coin,curr_coin);
+        }
+
+        return res[i][j] = max_coin;
+    }
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size();
+        nums.insert(nums.begin(),1);
+        nums.insert(nums.end(),1);
+        memset(res,-1,sizeof(res));
+        return solve(nums,1,n);
+    }
+};

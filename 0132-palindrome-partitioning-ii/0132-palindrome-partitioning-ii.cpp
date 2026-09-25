6f1c1bdfd6e4e248e90c69abba61a0b09c2d4d92
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-26 02:08:51 +0530

    Time: 1261 ms (44.49%), Space: 9 MB (99.06%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    int n;
     bool ispalindrome(string &s,int i,int j){
         while(i<j){
             if(s[i] != s[j]) return false;
@@ -9,23 +8,23 @@ public:
         }
         return true;
     }
-    int res[2001];
-    int solve(string &s,int i){
-        if(i == n) return 0;
-        if(res[i] != -1) return res[i];
-        int min_cuts = 1e9;
-        for(int j=i;j<n;j++){
-            if(ispalindrome(s,i,j)){
-                int curr_cuts = 1 + solve(s,j+1);
-                min_cuts = min(min_cuts,curr_cuts);
-            }
-        }
-
-        return res[i] = min_cuts;
-    }
     int minCut(string s) {
-        n = s.length();
-        memset(res,-1,sizeof(res));
-        return solve(s,0)-1;
+        int n = s.length();
+        vector<int> dp(n+2,0);
+        for(int i=n;i>=0;i--){
+            if(i == n){
+                dp[n] = 0;
+                continue;
+            }
+            int min_cuts = 1e9;
+            for(int j=n;j>=i;j--){
+                if(ispalindrome(s,i,j)){
+                    int curr_cuts = 1 + dp[j+1];
+                    min_cuts = min(min_cuts,curr_cuts);
+                }
+            }
+            dp[i] = min_cuts;
+        }
+        return dp[0] - 1;
     }
 };

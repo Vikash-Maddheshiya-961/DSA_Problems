21abfed777a9b71c816ce0440f93e02594849575
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-26 12:29:12 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.38%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-    vector<int> res;
-    int solve(int n){
-        if(n == 1) return res[1];
-        if(n == 2) return res[2];
-        if(res[n] != -1) return res[n];
-        res[n] = solve(n-1) + solve(n-2);
-        return res[n];
-    }
     int climbStairs(int n) {
-        res.assign(n+2,-1);
-        res[1] = 1;
-        res[2] = 2;
-        return solve(n);
+        if(n <= 2) return n;
+        int prev = 1;
+        int curr = 2;
+
+        for(int i=3;i<=n;i++){
+            int nxt = prev + curr;
+            prev = curr;
+            curr = nxt;
+        }
+
+        return curr;
     }
 };

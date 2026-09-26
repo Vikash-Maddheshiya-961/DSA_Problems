00b149212082b6832cbbeee7586a7519b96f9c35
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-26 13:29:43 +0530

    Time: 73 ms (78.58%), Space: 36.2 MB (70.28%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.cpp
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -1,29 +1,30 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
+    int n;
+    map<int,int> mp;
     int res[2001][2001];
-    bool solve(vector<int>& stones,int currstone,int prevjump){
-        int n = stones.size();
-        if(currstone == n-1){
-            return true;
-        }
-        if(res[currstone][prevjump] != -1) return res[currstone][prevjump];
+    bool solve(vector<int>& stones,int idx,int prev_jmp){
+        if(idx == n-1) return true;
+        if(res[idx][prev_jmp]!=-1) return res[idx][prev_jmp];
         int result = false;
-        for(int nextjump = prevjump-1; nextjump <= prevjump+1; nextjump++){
-            if(nextjump <= 0) continue;
-            int nextstone = stones[currstone] + nextjump;
+        for(int nxt_jmp = prev_jmp - 1; nxt_jmp <= prev_jmp + 1; nxt_jmp++){
+            if(nxt_jmp <= 0) continue;
+            int nextstone = stones[idx] + nxt_jmp;
             if(mp.find(nextstone) != mp.end()){
-                result = result || solve(stones,mp[nextstone],nextjump);
+                result = result || solve(stones,mp[nextstone],nxt_jmp);
             }
         }
-        return res[currstone][prevjump] = result;
+        
+
+        return res[idx][prev_jmp] = result;
     }
     bool canCross(vector<int>& stones) {
-        int n = stones.size();
+        if(stones[1] != stones[0] + 1) return false;
+        n = stones.size();
         for(int i=0;i<n;i++){
             mp[stones[i]] = i;
         }
-        memset(res, -1, sizeof(res));
+        memset(res,-1,sizeof(res));
         return solve(stones,0,0);
     }
 };

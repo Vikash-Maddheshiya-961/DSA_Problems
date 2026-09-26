604a6163605c09b6f3567c135ca9f059232bd32b
Vikash Maddeshiya <155875648+Vikash-Maddheshiya-961@users.noreply.github.com>
2026-09-26 10:06:42 +0530

    Time: 0 ms (100%), Space: 7.7 MB (65.11%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    int res[31];
-    int fibo(int n){
-        if(n <= 1) return n;
-        if(res[n] != -1) return res[n];
-        return res[n] = fibo(n-1) + fibo(n-2);
-    }
     int fib(int n) {
-        memset(res,-1,sizeof(res));
-        return fibo(n);
+        if(n <= 1) return n;
+        int prev = 0;
+        int curr = 1;
+
+        for(int i=2;i<=n;i++){
+            int nxt = prev + curr;
+            prev = curr;
+            curr = nxt;
+        }
+
+        return curr;
     }
 };
